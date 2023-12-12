0d0ddac2cf25e82a5fed451c6a2e61f2e5c2390e
andyp714 <85464699+andyp714@users.noreply.github.com>
2023-12-11 21:38:57 -0500

    add  js and  more questions

---
+++ processResults.js
@@ -0,0 +1,6 @@
+function getResults() {
+    document.getElementById("resultCheck").style.display = "block";
+    let q1Value = document.getElementById("q1a1").value
+    document.getElementById("resultCheck").innerHTML = q1Value
+
+}
